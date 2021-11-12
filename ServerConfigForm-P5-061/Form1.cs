9b3cbe5cc42a83bd60722315f622c32e6aaa6143
Bagus Rinu Pangayom <bagusrp2001@gmail.com>
2021-11-13 06:52:58 +0700

    Add Files

---
+++ ServerConfigForm-P5-061/Form1.cs
@@ -0,0 +1,47 @@
+﻿using ServiceMtk_P1_061;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ServerConfigForm_P5_061
+{
+    public partial class Form1 : Form
+    {
+
+        private ServiceHost Host;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void buttonOn_Click(object sender, EventArgs e)
+        {
+            Host = new ServiceHost(typeof(Matematika));
+
+            labelOnOrOff.Text = "Server ON";
+            labelKet.Text = "Klik Off untuk Mematikan Server";
+            Host.Open();
+            buttonOff.Enabled = true;
+            buttonOn.Enabled = false;
+
+        }
+
+        private void buttonOff_Click(object sender, EventArgs e)
+        {
+            buttonOff.Enabled = false;
+            buttonOn.Enabled = true;
+            Host.Close();
+            labelKet.Text = "Klik Untuk Menjalankan Server";
+            labelOnOrOff.Text = "Server Off";
+        }
+    }
+}
